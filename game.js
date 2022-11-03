0178cf362f1de7987a62b756640dd6933b7be288
prithvi.yadav <prithvi.yadav.ug21@nsut.ac.in>
2022-11-03 23:19:55 +0530

    modified:   game.js

--- game.js
+++ game.js
@@ -32,7 +32,7 @@ function enemyAttack(enemy) {
         enemy.removeClass("shooting");
         setTimeout(function () {
           enemy.removeClass("showing");
-        }, 150);
+        }, 200);
       }, 200);
     }
   }, 1000);
@@ -51,11 +51,7 @@ function updateHealth(points) {
   health = points;
   var healthBar = $("#healthbar")[0]; //this returnins an array so we cant change width of element which is residing at 0 index but we can addclass/delete easily without any problem
   healthBar.style.width = points + "%";
-
-  if (health < 1) {
-    health = 0;
-    var healthBar = $("#healthbar")[0];
-    healthBar.style.width = points + "%";
+  if (health == 0) {
     setTimeout(function () {
       if (livingEnemies().length) {
         alert("GAME OVER");
